7e0041e9c157963335a3a014ab1645488123f770
dmitry <dmitry.ema@gmail.com>
2021-01-26 16:56:53 +0200

    Turtles position tracker

--- tracking/trackers.py
+++ tracking/trackers.py
@@ -87,5 +87,133 @@ class ProgressionTracker(Tracker):
     pass
 
 
-class TurtlesTracker(Tracker):
-    pass
+class TurtleTracker(TakeStopTracker):
+    """
+    Our modifiaction of turtles money managenet algo
+    """
+    def __init__(self,
+                 account_size, dollar_per_point, max_units=4, risk_capital_pct=0.01, reinvest_pnl_pct=0,
+                 contract_size=100, max_allowed_contracts=200,
+                 atr_timeframe='1d', pull_stops_on_incr=False, after_lose_only=False, debug=False):
+        """
+        :param accoun_size: starting amount in USD
+        :param dollar_per_point: price of 1 point (for example 12.5 for ES mini) if none crypto sizing would be used
+        :param max_untis: maximal number of position inreasings
+        :param risk_capital_pct: percent of capital in risk (0.01 is 1%)
+        :param reinvest_pnl_pct: percent of reinvestment pnl to trading (default is 0)
+        :param contract_size: contract size in USD
+        :param max_allowed_contracts: maximal allowed contracts to trade
+        :param atr_timeframe: timeframe of ATR calculations
+        :param pull_stops_on_incr: if true it pull up stop on position's increasing
+        :param after_lose_only: if true
+        :param debug: if true it prints debug messages
+        """
+        super().__init__(debug)
+        self.account_size = account_size
+        self.dollar_per_point = dollar_per_point
+        self.atr_timeframe = atr_timeframe
+        self.max_units = max_units
+        self.trading_after_lose_only = after_lose_only
+        self.pull_stops = pull_stops_on_incr
+        self.risk_capital_pct = risk_capital_pct
+        self.max_allowed_contracts = max_allowed_contracts
+        self.reinvest_pnl_pct = reinvest_pnl_pct
+        self.contract_size = contract_size
+
+        if dollar_per_point is None:
+            self.calculate_trade_size = self._calculate_trade_size_crypto
+
+    def initialize(self):
+        self.days = self.get_ohlc_series(self.atr_timeframe)
+        self.N = None
+        self.__TR_init_sum = 0
+        self._n_entries = 0
+        self._last_entry_price = np.nan
+
+    def _get_size_at_risk(self):
+        return (self.account_size + self.reinvest_pnl_pct * self._position.pnl) * self.risk_capital_pct
+
+    def _calculate_trade_size_crypto(self, direction, vlt, price):
+        price2 = price + direction * vlt
+        return np.clip(round(self._get_size_at_risk() / ((price2 / price - 1) * self.contract_size)),
+                       -self.max_allowed_contracts, self.max_allowed_contracts)
+
+    def _calculate_trade_size_on_dollar_cost(self, direction, vlt, price):
+        return min(self.max_units, round(self._get_size_at_risk() / (vlt * self.dollar_per_point))) * direction
+
+    def calculate_trade_size(self, direction, vlt, price):
+        return self._calculate_trade_size_on_dollar_cost(direction, vlt, price)
+
+    def on_quote(self, quote_time, bid, ask, bid_size, ask_size, **kwargs):
+        daily, n_days = self.days, len(self.days)
+        today, yest = daily[1], daily[2]
+
+        if yest is None or today is None:
+            return
+
+        if daily.is_new_bar:
+            now_date = daily[0].time
+            TR = max(today.high - today.low, today.high - yest.close, yest.close - today.low)
+            if self.N is None:
+                if n_days <= 21:
+                    self.__TR_init_sum += TR
+                else:
+                    self.N = self.__TR_init_sum / 19
+            else:
+                self.N = (19 * self.N + TR) / 20
+
+        # increasing position size if possible
+        pos = self._position.quantity
+        if pos != 0 and self._n_entries < self.max_units:
+            n_2 = self.N / 2
+            if (pos > 0 and ask > self._last_entry_price + n_2) or (pos < 0 and bid < self._last_entry_price - n_2):
+                self._last_entry_price = ask if pos > 0 else bid
+                t_size = self.calculate_trade_size(np.sign(pos), self.N, self._last_entry_price)
+                self._n_entries += 1
+
+                # increase inventory
+                self.trade(quote_time, pos + t_size)
+
+                # average position price
+                avg_price = self._position.cost_usd / abs(self._position.quantity)
+
+                # pull stops
+                if self.pull_stops:
+                    self.stop_at(quote_time, avg_price - self.N * 2 * np.sign(pos))
+
+                self.debug(
+                    f"\t[{quote_time}] -> [#{self._n_entries}] {self._instrument} <{avg_price:.2f}> increasing to "
+                    f"{pos + t_size} @ {self._last_entry_price} x {self.stop:.2f}")
+
+        # call stop/take tracker to process sl/tp if need
+        super().on_quote(quote_time, bid, ask, bid_size, ask_size, **kwargs)
+
+    def on_signal(self, signal_time, signal, quote_time, bid, ask, bid_size, ask_size):
+        if self.N is None:
+            return None
+
+        s_type, s_direction, t_size = abs(signal), np.sign(signal), None
+        position = self._position.quantity
+
+        # when we want to enter position
+        if position == 0 and s_type == 1:
+            if not self.trading_after_lose_only or self.last_triggered_event != 'stop':
+                self._last_entry_price = ask if s_direction > 0 else bid
+                t_size = self.calculate_trade_size(s_direction, self.N, self._last_entry_price)
+                self.stop_at(signal_time, self._last_entry_price - self.N * 2 * s_direction)
+                self.last_triggered_event = None
+                self._n_entries = 1
+                self.debug(
+                    f'\t[{signal_time}] -> [#{self._n_entries}] {self._instrument} {t_size} @ '
+                    f'{self._last_entry_price:.2f} x {self.stop:.2f}')
+
+        # when we got to exit signal
+        if (position > 0 and signal == -2) or (position < 0 and signal == +2):
+            self.last_triggered_event = 'take'
+            self._n_entries = 0
+            t_size = 0
+            self.debug(f'[{signal_time}] -> Close in profit {self._instrument} @ {bid if position > 0 else ask}')
+            self.times_to_take.append(signal_time - self._last_trade_time)
+            self.n_takes += 1
+
+        return t_size
