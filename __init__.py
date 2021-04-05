d5bfe14b8417326186043e8d8dcec13324ce7c2d
dmitry <dmitry.ema@gmail.com>
2021-04-05 18:08:53 +0300

    Added handy imports to init 2

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from qlearn.core.base import Filter, MarketDataComposer, signal_generator
 from qlearn.core.pickers import SingleInstrumentPicker, PortfolioPicker
 from qlearn.core.metrics import ForwardDirectionScoring, ForwardReturnsSharpeScoring, ReverseSignalsSharpeScoring
 
+from qlearn.core.generators import RangeBreakoutDetector, PivotsBreakoutDetector
 from qlearn.core.transformers import RollingRange, FractalsRange, Pivots
 from qlearn.core.filters import AdxFilter, AcorrFilter, VolatilityFilter
 from qlearn.tracking.trackers import (
@@ -11,4 +12,5 @@ from qlearn.tracking.trackers import (
 
 # some helpers
 from qlearn.core.utils import search_params_init, debug_output, permutate_params
+from qlearn.core.generators import crossup, crossdown
 from sklearn.pipeline import make_pipeline
