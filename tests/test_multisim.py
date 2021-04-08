1ab3e5e18ed4507f86971f4a14d0549450cef788
dmitry <dmitry.ema@gmail.com>
2021-04-08 13:21:35 +0300

    Fixed test

--- tests/test_multisim.py
+++ tests/test_multisim.py
@@ -35,18 +35,17 @@ class Test(TestCase):
         simulations_report(r, 1000, only_report=True)
 
     def test_simulation_fixed(self):
-        data = pd.read_csv('data/ES.csv.gz', parse_dates=True, index_col=['time'])
-
         m1 = MarketDataComposer(make_pipeline(RollingRange('1H', 12), RangeBreakoutDetector()),
                                 SingleInstrumentPicker(), debug=True)
 
         r = simulation({
             'exp1 [FIXED TRADER]': [m1, FixedTrader(10, 30, 10, 1)]
-        }, {'ES': data}, 'forex', 'Test1')
-        # debug_output(r.results[0].portfolio, 'Portfolio')
-
-        # self.assertAlmostEqual(24.50, r.results[0].portfolio['ES_PnL'].sum())
-        # self.assertAlmostEqual(46.75, r.results[1].portfolio['ES_PnL'].sum())
-        print(r.results[0].trackers_stat)
+        }, {'ES': self.data}, 'forex', 'Test1')
 
         simulations_report(r, 1000, only_report=True)
+
+        print(r.results[0].trackers_stat)
+        self.assertEqual(3, r.results[0].trackers_stat['ES']['takes'])
+        self.assertEqual(41, r.results[0].trackers_stat['ES']['stops'])
+        self.assertAlmostEqual(1255.0, r.results[0].portfolio['ES_PnL'].sum())
+
