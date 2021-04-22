e50afa9fc6fee80639335141eb1013e2a811468a
dmitry <dmitry.ema@gmail.com>
2021-04-22 22:32:36 +0300

    Gridsearch helper

--- __init__.py
+++ __init__.py
@@ -11,5 +11,6 @@ from qlearn.tracking.trackers import (
 # some helpers
 from qlearn.core.utils import ls_params, debug_output, permutate_params
 from qlearn.core.generators import crossup, crossdown
+from qlearn.core.mlhelpers import gridsearch
 from sklearn.pipeline import make_pipeline
 from qlearn.simulation.multisim import simulation
